3bc3057796b031ee66718f365a532e567afea45a
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2022-04-27 09:03:13 +0300

    0.0.6

--- src/main.py
+++ src/main.py
@@ -1,19 +1,26 @@
+from prettytable import PrettyTable
+
 from report import build_report
 
 ABBREIVIATIONS = 'abbreviations.txt'
 
 
-
 def print_report(file):
     count = 1
+    table = PrettyTable()
+    table.field_names = ["Number", "Driver name", "Team name", "Lap time"]
     for key, value in build_report(file).items():
-        if count <= 15:
-            print(f'{count}. {value[0]}   |  {value[1]}    |  {value[2]}')
+        if count < 16:
+            table.add_row([count, value[0], value[1], value[2]])
+            count += 1
+        elif count == 16:
+            table.add_row(["-" * 6, "-" * 17, "-" * 25, "-" * 8])
+            table.add_row([count, value[0], value[1], value[2]])
             count += 1
         else:
-            # print('-' * 150)
-            print(f'{count}. {value[0]}   |  {value[1]}    |  {value[2]}')
+            table.add_row([count, value[0], value[1], value[2]])
             count += 1
+    print(table)
 
 
 print_report(ABBREIVIATIONS)
